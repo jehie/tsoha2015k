d7fdab5c46139e07ff3b27dc969881f0593da32a
Jesse Hietanen <jesse.hietanen@helsinki.fi>
2015-05-31 12:47:30 +0300

    heroku

--- src/main/java/Ostoskassi/Ostoskassi/database/GetPostGreSQLConnection.java
+++ src/main/java/Ostoskassi/Ostoskassi/database/GetPostGreSQLConnection.java
@@ -5,6 +5,8 @@
  */
 package Ostoskassi.Ostoskassi.database;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -15,15 +17,29 @@ import java.util.Properties;
  * @author Jesse
  */
 public class GetPostGreSQLConnection {
+//
+//    public Connection getConnection() throws ClassNotFoundException, SQLException {
+//        Class.forName("org.postgresql.Driver");
+//
+//        String url = "jdbc:postgresql://ec2-54-204-35-248.compute-1.amazonaws.com/dc7900e88qvbop";
+//        Properties props = new Properties();
+//        //props.setProperty("ssl", "true");
+//        props.setProperty("user", "rmalctxvdhbfnz");
+//        props.setProperty("password", "zD4_RBij1e-9rpoF8wF7kXOXNz");
+//        Connection conn = DriverManager.getConnection(url, props);
+//        return conn;
+//
+//    }
+    
+    public static Connection getConnection() throws URISyntaxException, SQLException {
+    URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
+    String username = dbUri.getUserInfo().split(":")[0];
+    String password = dbUri.getUserInfo().split(":")[1];
+    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
-        String url = "jdbc:postgresql://localhost/johietan";
-        Properties props = new Properties();
-        //props.setProperty("ssl", "true");
-        Connection conn = DriverManager.getConnection(url, props);
-        return conn;
-
-    }
+    return DriverManager.getConnection(dbUrl, username, password);
+}
+    
+    
 }
--- src/main/java/Ostoskassi/OstoskassiApplication.java
+++ src/main/java/Ostoskassi/OstoskassiApplication.java
@@ -1,6 +1,7 @@
 package Ostoskassi;
 
 import Ostoskassi.Ostoskassi.database.GetPostGreSQLConnection;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -11,12 +12,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OstoskassiApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, URISyntaxException {
         String webPort = System.getenv("PORT");
         if (webPort == null || webPort.isEmpty()) {
             webPort = "8080";
         }
         System.setProperty("server.port", webPort);
         SpringApplication.run(OstoskassiApplication.class, args);
+        
+        
+        GetPostGreSQLConnection conn = new GetPostGreSQLConnection();
+        Connection connection = conn.getConnection();
+        System.out.println(connection.getSchema());
     }
 }
