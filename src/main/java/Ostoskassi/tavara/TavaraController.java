e60a6b8a92b384cab2725dd53b876d69fafb0fc1
Jesse Hietanen <jesse.hietanen@helsinki.fi>
2015-06-06 19:34:56 +0300

    Update tavara

--- src/main/java/Ostoskassi/tavara/TavaraController.java
+++ src/main/java/Ostoskassi/tavara/TavaraController.java
@@ -70,7 +70,7 @@ public class TavaraController {
     public String updateTavara(@PathVariable int Id, @RequestParam String kuvaus) throws URISyntaxException, SQLException {
         Connection connection = GetPostGreSQLConnection.getConnection();
         Statement statement = connection.createStatement();
-        statement.execute("UPDATE Tavara  SET kuvaus=TESTI WHERE id=" + Id);
+        statement.execute("UPDATE Tavara  SET kuvaus='"+kuvaus+"' WHERE id=" + Id);
         connection.close();
         //System.out.println(kuvaus);
         return "updated";
