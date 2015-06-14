74a5f611bf1580b37090e3fedceb4fe6031aff87
Jesse Hietanen <jesse.hietanen@helsinki.fi>
2015-06-14 10:59:33 +0300

    paiv

--- src/main/java/Ostoskassi/Ateria/AteriaController.java
+++ src/main/java/Ostoskassi/Ateria/AteriaController.java
@@ -71,6 +71,7 @@ public class AteriaController {
         Connection connection = GetPostGreSQLConnection.getConnection();
         Statement statement = connection.createStatement();
         //statement.execute("UPDATE Ateria SET kuvaus='" + kuvaus + ", hinta=" + hinta + " WHERE id=" + Id);
+        statement.execute("UPDATE Ateria SET kuvaus='"+kuvaus+"', hinta="+hinta+" WHERE id=" + Id);
         connection.close();
         //System.out.println(kuvaus);
         return "updated";
