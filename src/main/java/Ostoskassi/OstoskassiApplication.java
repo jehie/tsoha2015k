23b4fa2ee0fc27f96c003bd98f3a9c6c8d95b824
Jesse Hietanen <jesse.hietanen@helsinki.fi>
2015-06-25 17:46:56 +0300

    vikaa

--- src/main/java/Ostoskassi/OstoskassiApplication.java
+++ src/main/java/Ostoskassi/OstoskassiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Luokka Spring-sovelluksen käynnistämiseen
+ * Luokka Spring-sovelluksen käynnistämiseen.
  *
  * @author Jesse
  */
